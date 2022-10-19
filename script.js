143ae97b23fa5149276409608ff34044e1c2e3c5
MonarkGondaliya <mgondaliya@hawk.iit.edu>
2022-10-19 01:48:48 -0500

    Added image and modify css file

--- script.js
+++ script.js
@@ -8,16 +8,17 @@ function TipCalculate() {
     console.log('Tip Calculator');
     let billAmount = Number(document.getElementById('idBillTotal').value);
     let idTipPercentage = Number(document.getElementById('idTipPercentage').value);
-    if (billAmount < 0 || billAmount < 0 || isNaN(billAmount) ) {
+    if (billAmount < 0 || billAmount == '' || isNaN(billAmount)) {
         alert("Total Bill value must be a valid positive number");
         document.getElementById('idBillTotal').value = '';
         document.getElementById('idTipAmount').value = '';
         document.getElementById('idTotal').value = '';
     }
     else {
-        let Tip = (idTipPercentage/100) * billAmount;
-        let Total = ((idTipPercentage/100) * billAmount + billAmount )
-        document.getElementById('idTipAmount').value = Tip;
-        document.getElementById('idTotal').value = Total;
+        let Tip = (idTipPercentage / 100) * billAmount;
+        let Total = ((idTipPercentage / 100) * billAmount + billAmount)
+        document.getElementById('idTipAmount').value = Tip.toFixed(2);
+        document.getElementById('idTotal').value = Total.toFixed(2);
     }
 }
+
